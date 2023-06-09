b594ca41fc46da4c3183ca117fd642693cbc9541
hagan23 <haganemmanuel23@gmail.com>
2023-06-09 12:54:18 +0000

    Update runtime.bundle.js

--- dist/runtime.bundle.js
+++ dist/runtime.bundle.js
@@ -158,7 +158,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkleaderboard"] = self["webpackChunkleaderboard"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkjavascriptcapstone"] = self["webpackChunkjavascriptcapstone"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
